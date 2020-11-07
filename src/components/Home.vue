70cc806d42d6d29a45cfd70d1b6c44904c79f8e8
zengjunwei1 <347350971@qq.com>
2020-11-07 15:36:12 +0800

    12

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home"> 
+    Home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: ' '
+    }
+  }
+}
+</script>
+ 
+<style scoped> 
+</style>
